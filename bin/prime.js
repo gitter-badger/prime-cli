9971db40e1052e5b05a0b47297987737958c4a05
Muhammad Sayuti <muhammad.sayuti94@gmail.com>
2017-02-10 17:49:32 +0800

    remove unnecessary variable.

--- bin/prime.js
+++ bin/prime.js
@@ -10,26 +10,6 @@ const logging = require('plylog');
 const logger = logging.getLogger('cli');
 updateNotifier({pkg: packageJson}).notify();
 
-let list = (directory,options)  => {
-    const cmd = 'ls';
-    let params = [];
-    
-    if (options.all) params.push("a");
-    if (options.long) params.push("l");
-    let parameterizedCommand = params.length 
-                                ? cmd + ' -' + params.join('') 
-                                : cmd ;
-    if (directory) parameterizedCommand += ' ' + directory ;
-    
-    let output = (error, stdout, stderr) => {
-        if (error) console.log(chalk.red.bold.underline("exec error:") + error);
-        if (stdout) console.log(chalk.green.bold.underline("Result:") + stdout);
-        if (stderr) console.log(chalk.red("Error: ") + stderr);
-    };
-    
-    exec(parameterizedCommand,output);
-    
-};
 resolve('prime-cli', {basedir: process.cwd()}, function(error, path) {
   let lib = path ? require(path) : require('../lib/prime-cli');
   let args = process.argv.slice(2);
