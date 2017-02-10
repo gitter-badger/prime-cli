c5c8a7b815fb5bb95fddb8e2ecb82f65cff696b0
Muhammad Sayuti <muhammad.sayuti94@gmail.com>
2017-02-10 20:31:16 +0800

    cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,4 @@
-/**
- * @license
- * Copyright (c) 2016 The Prime Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://nerdiex.github.io/Prime/AUTHORS.txt
- * The complete set of contributors may be found at http://nerdiex.github.io/Prime/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
+
 'use strict';
 
 const depcheck = require('depcheck');
